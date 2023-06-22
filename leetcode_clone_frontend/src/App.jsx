ed440d55b4b28a292d569d779d8a4cff2a3a7250
PrathumP <115390367+PrathumP@users.noreply.github.com>
2023-06-23 01:27:22 +0530

    Fixing the errors

--- leetcode_clone_frontend/src/App.jsx
+++ leetcode_clone_frontend/src/App.jsx
@@ -1,12 +1,10 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 
-const problems = [{
+ {/*const problems = [{
   title: "201. Bitwise AND of Numbers Range",
   difficulty: "Medium",
   acceptance: "42%"
@@ -26,29 +24,54 @@ const problems = [{
       acceptance: "42%"
   }];
 
+  function problemStatement(props){
+    const title = props.title;
+    const difficulty = props.difficulty;
+    const acceptance = props.acceptance;
+    return (
+    <tr>
+      <td> {title} </td>
+      <td> {difficulty} </td>
+      <td> {acceptance} </td>
+      </tr> 
+      );
+  }   */}
+
 function App() {
   return (
+    <div className="App"> <h1> Hello bhai</h1></div>,
     <Router>
-      <Switch>
-        <Route exact path ="/" component ={Home} />
-        <Route exact path ="/login" component ={Login} />
-        <Route exact path ="/signup" component ={Signup} />
-        <Route exact path ="/problemset/all" component ={problems} />
-      </Switch>
+      <Routes>
+      <Route path="/" element={<Home />}></Route>
+      <Route path="/login" element={<Login />}></Route>
+      {/*  <Route exact path ="/signup" component ={Signup} />
+        <Route exact path ="/problemset/all" component ={problems} /> */}
+      </Routes>
     </Router>
-  )
+  );
 }
 
-function problemStatement(props){
-  const title = props.title;
-  const difficulty = props.difficulty;
-  const acceptance = props.acceptance;
-  return 
-  <tr>
-    <td> {title} </td>
-    <td> {difficulty} </td>
-    <td> {acceptance} </td>
-    </tr>
+function Home() {
+  return (
+    <div>
+      <h1>Welcome to the Home Page</h1>
+      {/* Add your content here */}
+    </div>
+  );
+}
+
+function Login(){
+  return (
+  <div>
+    <h1> Log In</h1>
+    <div>
+      <input type ="text" placeholder="E-mail"></input>
+      <input type ="text" placeholder="Password"></input>
+    </div>
+    <div>
+      <button> Log in</button>
+    </div>
+  </div> );
 }
 
 export default App
