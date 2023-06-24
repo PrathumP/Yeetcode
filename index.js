430e5b206174d269f0df93bd28605b574891d33d
PrathumP <115390367+PrathumP@users.noreply.github.com>
2023-06-25 02:23:13 +0530

    Added dependencies + more routes

--- index.js
+++ index.js
@@ -1,99 +1,221 @@
-const express = require('express')
-const app = express()
-const port = 3000
-const jwt = require('jsonwebtoken')
+const express = require("express");
+const app = express();
+const port = 3000;
+var jwt = require("jsonwebtoken");
+const { auth } = require("./middleware");
+let USER_ID_COUNTER = 1;
+const USERS = [
+  {
+    email : "evsdrvs45",
+    password : "1234244"
+  }
+];
+const JWT_SECRET = "secret";
+const bodyParser = require("body-parser");
+var jsonParser = bodyParser.json();
+var urlencodedParser = bodyParser.urlencoded({ extended: false });
+const cors = require("cors");
+app.use(cors());
+app.use(jsonParser);
 
-const USERS =[
-    { email: 'user1@example.com', password: 'password1' },
-    { email: 'user2@example.com', password: 'password2' }
+const PROBLEMS = [
+  {
+    problemId: "1",
+    title: "401. Bitwise AND of Numbers Range",
+    difficulty: "Medium",
+    acceptance: "42%",
+    description:
+      "Given two integers left and right that represent the range [left, right], return the bitwise AND of all numbers in this range, inclusive.",
+    exampleIn: "left = 5, right = 7",
+    exampleOut: "4",
+  },
+  {
+    problemId: "2",
+    title: "205. Add two numbers",
+    difficulty: "Medium",
+    acceptance: "41%",
+    description:
+      "Given two numbers, add them and return them in integer range. use MOD=1e9+7",
+    exampleIn: "a = 100 , b = 200",
+    exampleOut: "300",
+  },
+  {
+    problemId: "3",
+    title: "202. Happy Number",
+    difficulty: "Easy",
+    acceptance: "54.9%",
+    description: "Write an algorithm to determine if a number n is happy.",
+    exampleIn: "n = 19",
+    exampleOut: "true",
+  },
+  {
+    problemId: "4",
+    title: "203. Remove Linked List Elements",
+    difficulty: "Hard",
+    acceptance: "42%",
+    description: "Given number k , removed kth element",
+    exampleIn: "list: 1->2->3 , k=2",
+    exampleOut: "1->3",
+  },
+  {
+    problemId: "5",
+    title: "201. Bitwise AND of Numbers Range",
+    difficulty: "Medium",
+    acceptance: "42%",
+    description:
+      "Given two integers left and right that represent the range [left, right], return the bitwise AND of all numbers in this range, inclusive.",
+    exampleIn: "left = 5, right = 7",
+    exampleOut: "4",
+  },
+  {
+    problemId: "6",
+    title: "205. Add two numbers",
+    difficulty: "Medium",
+    acceptance: "41%",
+    description:
+      "Given two numbers, add them and return them in integer range. use MOD=1e9+7",
+    exampleIn: "a = 100 , b = 200",
+    exampleOut: "300",
+  },
+  {
+    problemId: "7",
+    title: "202. Happy Number",
+    difficulty: "Easy",
+    acceptance: "54.9%",
+    description: "Write an algorithm to determine if a number n is happy.",
+    exampleIn: "n = 19",
+    exampleOut: "true",
+  },
+  {
+    problemId: "8",
+    title: "203. Remove Linked List Elements",
+    difficulty: "Hard",
+    acceptance: "42%",
+    description: "Given number k , removed kth element",
+    exampleIn: "list: 1->2->3 , k=2",
+    exampleOut: "1->3",
+  },
 ];
 
-const SUBMISSIONS =[];
+const SUBMISSIONS = [];
 
-function generateAuthToken(user) {
-    const secretKey = 'your secret-key'; // Replace with your own secret key
-  
-    // Create the token with user data and secret key
-    const token = jwt.sign({ userId: user.id, email: user.email }, secretKey, { expiresIn: '1h' });
-  
-    return token;
-} 
-
-const QUESTIONS = [{
-    title : "Total sum",
-    description : " Given an array, return the sum of all the elements of the array",
-    test_Cases : [{
-        input : "[1,2,3,4,5,6]",
-        output : "21"
-    }]
-
-}];
-
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
-
-app.post('/signup', (req,res) => {
-    const { email, password} = req.body;
-
-    const USEREXISTS = USERS.some(user => user.email === email)
-
-    if (USEREXISTS){
-        res.status(409).json({message : "User with this email already exists"});
-    } else {
-        USERS.push({email,password});
-        res.status(200).json({ message : "Signup Successful"});
-    }
-})
-
-app.post('/login', (req,res) => {
-    const { email, password } = req.body;
-
-    const USEREXISTS = USERS.some(user => user.email === email);
-    const token = 'random token';
-    if (USEREXISTS){
-        if (!user || user.password !== password){
-        res.status(401).json({message : "Invalid email or password"});}
-        else {
-        const token = generateAuthToken(user);
-
-        res.status(200).json({ message :"Login successful"}, token);  
-        }
-    }
-})
-
-app.get('/questions', (req,res) =>{
-    res.send(USERS);
-})
-
-app.post('/submissions', (req,res) => {
-    const {problem,solution} =req.body;
-    const isaccepted = Math.random() > 0.5;
-    
-    const submission = {
-        problem,
-        solution,
-        isaccepted,
-    }
-
-    SUBMISSIONS.push(submission);
-
-    if (isaccepted){
-        res.status(200).json({message : "Solution accepted"})
-    }
-    else {
-        res.status(400).json({message : "Solution rejected"})
-    }
+app.get("/", (req, res) => {
+  res.json({
+    msg: "hello world",
+  });
 });
 
-app.get('/submissions', (req,res)=>{
-    res.send(SUBMISSIONS)
-})
+app.get("/problems", (req, res) => {
+  const filteredProblems = PROBLEMS.map((x) => ({
+    problemId: x.problemId,
+    difficulty: x.difficulty,
+    acceptance: x.acceptance,
+    title: x.title,
+  }));
 
-app.get('/route2', (req, res) => {
-    res.send('Hello World! from route 2')
-  })
+  res.json({
+    problems: filteredProblems,
+  });
+});
+
+app.get("/problem/:id", (req, res) => {
+  const id = req.params.id;
+
+  const problem = PROBLEMS.find((x) => x.problemId === id);
+
+  if (!problem) {
+    return res.status(411).json({});
+  }
+
+  res.json({
+    problem,
+  });
+});
+
+app.get("/me", auth, (req, res) => {
+  const user = USERS.find((x) => x.id === req.userId);
+  res.json({ email: user.email, id: user.id });
+});
+
+app.get("/submissions/:problemId", auth, (req, res) => {
+  const problemId = req.params.problemId;
+  const submissions = SUBMISSIONS.filter(
+    (x) => x.problemId === problemId && x.userId === req.userId
+  );
+  res.json({
+    submissions,
+  });
+});
+
+app.post("/submission", auth, (req, res) => {
+  const isCorrect = Math.random() < 0.5;
+  const problemId = req.body.problemId;
+  const submission = req.body.submission;
+
+  if (isCorrect) {
+    SUBMISSIONS.push({
+      submission,
+      problemId,
+      userId: req.userId,
+      status: "AC",
+    });
+    return res.json({
+      status: "AC",
+    });
+  } else {
+    SUBMISSIONS.push({
+      submission,
+      problemId,
+      userId: req.userId,
+      status: "WA",
+    });
+    return res.json({
+      status: "WA",
+    });
+  }
+});
+
+app.post("/signup", (req, res) => {
+  const email = req.body.email;
+  const password = req.body.password;
+  if (USERS.find((x) => x.email === email)) {
+    return res.status(403).json({ msg: "Email already exists" });
+  }
+
+  USERS.push({
+    email,
+    password,
+    id: USER_ID_COUNTER++,
+  });
+
+  return res.json({
+    msg: "Success",
+  });
+});
+
+app.post("/login", (req, res) => {
+  const email = req.body.email;
+  const password = req.body.password;
+  const user = USERS.find((x) => x.email === email);
+
+  if (!user) {
+    return res.status(403).json({ msg: "User not found" });
+  }
+
+  if (user.password !== password) {
+    return res.status(403).json({ msg: "Incorrect password" });
+  }
+
+  const token = jwt.sign(
+    {
+      id: user.id,
+    },
+    JWT_SECRET
+  );
+
+  return res.json({ token });
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
