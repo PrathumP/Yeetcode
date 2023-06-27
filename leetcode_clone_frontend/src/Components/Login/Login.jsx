dfe61752e6d60c935dfaa53fc7320f388991a3ad
PrathumP <115390367+PrathumP@users.noreply.github.com>
2023-06-27 22:22:21 +0530

    Added Axios

---
+++ leetcode_clone_frontend/src/Components/Login/Login.jsx
@@ -0,0 +1,61 @@
+import React from "react";
+import axios from "axios";
+import { useState } from "react";
+import "./Login.css";
+import { backendUrl } from "../../constants.js";
+
+const Login = () => {
+  const [email, setEmail] = useState("");
+	const [password, setPassword] = useState("");
+  return (
+		<div id="login" className="flex-col">
+			<h1>Login</h1>
+			<div className="signup-form">
+				<div className="subform">
+					<label htmlFor="email">Email: </label>
+					<input
+						onChange={(e) => {
+							setEmail(e.target.value);
+						}}
+						type="text"
+						name="email"
+						placeholder="Your Email"
+					/>
+				</div>
+        <div className="subform">
+					<label htmlFor="password">Password: </label>
+					<input
+						onChange={(e) => setPassword(e.target.value)}
+						type="text"
+						name="password"
+						placeholder="Your Password"
+					/>
+				</div>
+        <button
+					type="submit"
+					id="test"
+					onClick={async (e) => {
+						axios
+							.post(`${backendUrl}/login`, {
+								email: email,
+								password: password,
+							})
+							.then(function (response) {
+								localStorage.setItem(
+									"token",
+									response.data.token
+								);
+							})
+							.catch(function (error) {
+								console.log(error);
+							});
+					}}
+				>
+					Login
+				</button>
+			</div>
+		</div>
+	);
+};
+
+export default Login;
--- leetcode_clone_frontend/src/Components/Signup.jsx
+++ leetcode_clone_frontend/src/Components/Signup.jsx
@@ -1,8 +1,7 @@
 import React, { useState } from "react";
-import { Link } from "react-router-dom";
-
 import "./Signup.css";
 import { backendUrl } from "../../constants.js";
+
 const Signup = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
