b78dc86cbf2f160a432f41350c59918452a5f625
PrathumP <115390367+PrathumP@users.noreply.github.com>
2023-07-19 00:28:02 +0530

    fixed untracked files

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 var jwt = require("jsonwebtoken");
 //const { auth } = require("./middleware");
 
